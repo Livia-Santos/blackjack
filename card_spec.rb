01e9f34370c07265878993895bcadfabe394619a
Livia_Santos <liviamelq@gmail.com>
2018-01-23 15:50:44 +1100

    implementedconstants for initialze Card

--- card_spec.rb
+++ card_spec.rb
@@ -20,5 +20,16 @@ describe Card do
     expect(@card).to respond_to(:show)
   end
 
+  it "'show' method returns true" do
+    expect(@card.show).to eq(true)
+  end
+
+  it "'suit' method returns 'Diamonds'" do
+    expect(@card.suit).to eq("Diamonds")
+  end
+
+  it "'rank' method returns '8'" do
+    expect(@card.rank).to eq("8")
+  end
 
 end
